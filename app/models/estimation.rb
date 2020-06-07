d994f3fd8a00b6ea944f3b98d7f496936dd57625
Stephanie Diep <stephaniediep@outlook.com>
2020-06-07 10:35:33 -0400

    Added controller and validation

--- app/models/estimation.rb
+++ app/models/estimation.rb
@@ -1,2 +1,6 @@
 class Estimation < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address, presence: true
+  validates :type, presence: true
 end
