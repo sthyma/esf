6c35fb4fcb2d5bab58569bfbfd43007ac2b4acc8
Stephanie Diep <stephaniediep@outlook.com>
2020-06-07 10:42:12 -0400

    Added routes, coded controller and added more validation

--- app/controllers/estimations_controller.rb
+++ app/controllers/estimations_controller.rb
@@ -1,2 +1,20 @@
 class EstimationsController < ApplicationController
+  def new
+    @estimation = Estimation.new
+  end
+
+  def create
+    @estimation = Estimation.new(estimation_params)
+
+    if @estimation.save
+      redirect_to root
+    else
+      render :new
+    end
+  end
+
+  private
+  def estimation_params
+    params.require(:estimation).permit(:first_name, :last_name, :address, :type)
+  end
 end
--- app/models/estimation.rb
+++ app/models/estimation.rb
@@ -2,5 +2,5 @@ class Estimation < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
-  validates :type, presence: true
+  validates :type, presence: true, inclusion: { in: ["Vendre dans les 3 prochains mois", "Vendre dans les 6 prochains mois", "Vendre dans les 12 prochains mois", "J'aimerais seulement avoir mon estimation"] }
 end
