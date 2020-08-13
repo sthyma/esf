854a62b49144a1f417a4c7450d02bc49eab72b32
Stephanie Diep <stephanie.diep002@gmail.com>
2020-08-13 13:24:55 -0400

    Migrated and started fixing form

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_203949) do
+ActiveRecord::Schema.define(version: 2020_08_13_172026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "estimations", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
     t.string "type_estimation"
+    t.string "full_name"
+    t.string "phone"
+    t.string "email"
   end
 
 end
