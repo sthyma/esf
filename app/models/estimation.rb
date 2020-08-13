60e8d005b3a09e2a876d069a06a62873ded6f19c
Stephanie Diep <61625854+sthyma@users.noreply.github.com>
2020-08-13 13:06:11 -0400

    Revert "Fixed form field and flow and started english version"

--- app/models/estimation.rb
+++ app/models/estimation.rb
@@ -1,7 +1,6 @@
 class Estimation < ApplicationRecord
-  validates :full_name, presence: true
-  validates :phone, presence: true, length: { minimum: 10, too_short: "Veuillez entrer au moins %{count} chiffres, incluant le code régional." }
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Veuillez entrer une adresse courriel valide." }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :address, presence: true
   validates :type_estimation, presence: true, inclusion: { in: ["Dans les 3 prochains mois", "Dans les 6 prochains mois", "Dans les 12 prochains mois", "J'aimerais seulement avoir mon estimation"] }
 
--- db/migrate/20200813162526_remove_columns_from_estimation.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnsFromEstimation < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :estimations, :first_name
-    remove_column :estimations, :last_name
-    add_column :estimations, :full_name, :string
-    add_column :estimations, :phone, :string
-    add_column :estimations, :email, :string
-  end
-end
